29298619b4fb9248e1076642a36961cf658ba89d
AlexB <alexandre.bouillot.1992@gmail.com>
2020-03-23 16:30:21 +0100

    qsdrfgsef

---
+++ src/app/_components/nav/nav.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { NbMenuItem } from '@nebular/theme';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+
+  items: NbMenuItem[];
+  
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.items = [
+      { title: 'Home', icon: 'home', link: '/home' },
+      
+    ]
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './_components/nav/nav.component';
+import { HomeComponent } from './_components/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbCardModule, NbInputModule, NbSidebarModule, NbMenuModule, NbButtonModule, NbDatepickerModule, NbSelectModule, NbDialogModule, NbToastrModule, NbListModule, NbActionsModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbCardModule,
+    NbInputModule,
+    NbSidebarModule.forRoot(),
+    NbMenuModule.forRoot(),
+    NbEvaIconsModule,
+    NbButtonModule,
+    NbDatepickerModule.forRoot(),
+    NbSelectModule,
+    NbDialogModule.forRoot(),
+    NbToastrModule.forRoot(),
+    NbListModule,
+    NbActionsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
